0fdc04d255288c175335bb7373921ae8e6cb59b4
miharbi91@gmail.com <miharbi91@gmail.com@a833f815-71fb-fab0-9083-0d637759828e>
2013-04-03 16:40:59 +0000

    Added Earth Module, which draws earth and stars. (Earth.c & Earth.h) Uploaded two textures, world.png & stars.png

---
+++ trunk/include/Earth.h
@@ -0,0 +1,50 @@
+
+#ifndef EARTH_H_INCLUDED
+#define EARTH_H_INCLUDED
+
+#include "includes.h"
+
+//defines a location is space.
+typedef struct {
+    float xRot;
+    float yRot;
+    float zRot;
+    float xTran;
+    float yTran;
+    float zTran;
+} Location;
+
+//the location of the countries in space.
+Location TUNISIA,EGYPT,FRANCE,NEWYORK,JAPAN;
+//Images for the map of earth, and the stars of space.
+IMAGE *world,*stars;
+//define travel mode. INTERPOLATED is better.
+typedef enum {LINEAR,INTERPOLATED} TravelMode;
+
+//Travels from a country to another.
+void DoEarth(Location start, Location end);
+//Advances the position of the traveling.
+//this function is called from DoEarth
+Location Travel(Location start,Location end,float percentage,TravelMode t);
+
+//A modified version of gluSphere to map small textures
+void gluSphere2(GLUquadric *qobj, GLdouble radius, GLint slices, GLint stacks,GLdouble CoordScale);
+
+//glu defines
+#define CACHE_SIZE	240
+
+#undef	PI
+#define PI	      3.14159265358979323846
+#define SIN sin
+#define COS cos
+
+
+struct GLUquadric {
+    GLint	normals;
+    GLboolean	textureCoords;
+    GLint	orientation;
+    GLint	drawStyle;
+};
+
+
+#endif // EARTH_H_INCLUDED
