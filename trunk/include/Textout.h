bc23f68ffbbffa7d5657259229ea6743b04ea634
miharbi91@gmail.com <miharbi91@gmail.com@a833f815-71fb-fab0-9083-0d637759828e>
2013-04-05 01:07:54 +0000

    First working version of textout. Current version use outlined text style.
    TODO : Filled up text style drawing.

--- trunk/include/Textout.h
+++ trunk/include/Textout.h
@@ -4,14 +4,15 @@
 #include "includes.h"
 
 typedef enum {SharpCurve} FONTS;
+typedef enum {NORMAL,CENTER_X,CENTER_Y,CENTER} FONTSTYLE;
 const char * const FontFiles[sizeof(FONTS)/sizeof(int)];
 FTGLfont **fontlist;
 
-
+//Load .ttf files
 int install_fonts();
-
-void draw_text(FONTS font,const char* text,int size,float x, float y);
-
+//draw text, using specific font,size, location and style.
+void draw_text(FONTS font,const char* text,float size,float x, float y, FONTSTYLE style);
+//free font memory
 void uninstall_fonts();
 
 #endif // TEXTOUT_H_INCLUDED
