b0d2094e391dc7e22b9d595923e040d9a4aa38ab
haithem.sboui@esprit.tn <haithem.sboui@esprit.tn@a833f815-71fb-fab0-9083-0d637759828e>
2013-04-26 15:36:15 +0000

    added display option

--- trunk/src/Setting.c
+++ trunk/src/Setting.c
@@ -17,3 +17,17 @@ void LoadSettings(){
     Height=get_config_int("graphics","Height",600);
     depth=get_config_int("graphics","depth",32);
 }
+
+GFX_MODE_LIST GetResolution()
+{
+  GFX_MODE_LIST * gfxlist;
+  int i;
+ gfxlist=get_gfx_mode_list(GFX_OPENGL_FULLSCREEN);
+    for(i=0; i<gfxlist->num_modes; i++)
+    {
+        printf("%dx%dx%d\n",gfxlist->mode[i].width,gfxlist->mode[i].height,gfxlist->mode[i].bpp);
+    }
+    printf("\nGraphics modes : %d\n",gfxlist->num_modes);
+
+return (* gfxlist);
+}
