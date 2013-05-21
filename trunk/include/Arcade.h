1679a55a089f7456d37e0ab59e5533e592d7bbe2
miharbi91@gmail.com <miharbi91@gmail.com@a833f815-71fb-fab0-9083-0d637759828e>
2013-05-21 18:36:49 +0000

    Cinematics

--- trunk/include/Arcade.h
+++ trunk/include/Arcade.h
@@ -3,10 +3,15 @@
 
 #include "includes.h"
 
+typedef enum {MOKHTAR,HAITHAM,BRAHIM,SALAH,WASSIM} PLAYERNAME;
+
 void Arcade();
 
 void Cinematic1();
 
+void CinematicWin(PLAYERNAME winner,PLAYERNAME looser);
+
+void CinematicVoice(PLAYERNAME player,PLAYERNAME next, LocationName nextmap);
 
 
 #endif // ARCADE_H_INCLUDED
