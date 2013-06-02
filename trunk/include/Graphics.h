1a06605206afe4e6d648589f50c4c7385fc057a9
miharbi91@gmail.com <miharbi91@gmail.com@a833f815-71fb-fab0-9083-0d637759828e>
2013-06-02 18:45:18 +0000

    Added draw_image_evil

--- trunk/include/Graphics.h
+++ trunk/include/Graphics.h
@@ -6,10 +6,16 @@
 //Flags to flip image when drawing
 typedef enum {NONE=0, HORIZONTAL=1,VERTICAL=2} flip;
 
+
+
 //Draw Image to screen
 void draw_image_ex(IMAGE *image, float x, float y, float w, float h,flip flips, float trans);
 //Draw Image with blue tint (freeze)
 void draw_image_freeze(IMAGE *image, float x, float y, float w, float h,flip flips, float trans);
+//Draw Image with evil tint
+void draw_image_evil(IMAGE *image, float x, float y, float w, float h,flip flips, float trans);
+//Basic draw image
+void draw_image(IMAGE *image, float x, float y, float w, float h,flip flips);
 //Blit buffer to screen and wait for next frame
 void next_frame();
 int Counter;
